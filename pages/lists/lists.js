6dd72c7db143f3823f77e268bc70ccd88f1c5a7e
liuqiuchen <2016968116@qq.com>
2016-12-08 23:45:49 +0800

    12/8

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -1,8 +1,36 @@
 // pages/lists/lists.js
 Page({
-  data:{},
+  data:{
+    newsList: [
+        {
+            id: 1,
+            img: '../../images/logo.png',
+            title: 'aaaaaaaa',
+            cTime: '2016-09-26 21:40'
+        },
+        {
+            id: 2,
+            img: '../../images/logo.png',
+            title: 'bbbbbbbb',
+            cTime: '2016-09-26 21:40'
+        },
+        {
+            id: 3,
+            img: '../../images/logo.png',
+            title: 'ccccc',
+            cTime: '2016-09-26 21:40'
+        },
+        {
+            id: 4,
+            img: '../../images/logo.png',
+            title: 'dddddd',
+            cTime: '2016-09-26 21:40'
+        }
+    ]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+      console.log(options);
   },
   onReady:function(){
     // 页面渲染完成
